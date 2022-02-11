69f60ec7e574e17136a243a2e37da1ca62259a40
SungMin Yoon <yoonmin.tech@gmail.com>
2022-02-11 22:37:36 +0900

    Solve-BOJ10430 2가지방법의 풀이

---
+++ Javascript/BOJ1-----/10430.js
@@ -0,0 +1,32 @@
+//나머지
+//(A+B)%C는 ((A%C) + (B%C))%C 와 같을까? (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+//세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+//첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
+//방법 1
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map((val) => +val);
+//map는 메서드는 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환한다.
+const [A, B, C] = input;
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
+
+//방법 2 number사용
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+const a = Number(input[0]);
+const b = Number(input[1]);
+const c = Number(input[2]);
+//질문: parseInt는 런타임 오류가 나오는데 Number은 이상이 없는 이유는 무엇일까?
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
